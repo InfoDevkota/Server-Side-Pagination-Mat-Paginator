ea80513a5b7fff9340257dcc1b36200a2b31ed53
Sagar Devkota <info@devkotasagar.com.np>
2020-03-31 21:57:23 +0545

    Initial Commit, Mat Paginator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +11,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  users: any[];
+  loading: boolean = true;
+  dataSource = new MatTableDataSource<any>();
+
   title = 'pagination';
+d
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+    this.getData();
+  }
+
+  getData(){
+    this.http.get(`http://localhost:3000/users`).subscribe((response: any) =>{
+
+    this.loading = false;
+    this.users = response.users;
+    this.dataSource = new MatTableDataSource<any>(this.users);
+    this.dataSource.paginator = this.paginator;
+
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    HttpClientModule,
+    
+    MatProgressSpinnerModule,
+    MatPaginatorModule,
+    MatTableModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
